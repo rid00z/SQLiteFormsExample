c913012bea8635890b361e05e96e5797375cdf18
Michael Ridland <rid00z@gmail.com>
2014-06-16 14:32:15 +1000

    android support

---
+++ Android/Dependancies/SQLiteFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using SQLite.Net;
+using System.IO;
+using SQLite.Net.Platform.XamarinAndroid;
+
+namespace SQLiteExample.Android.Dependancies
+{
+    public class SQLiteFactory : ISQLiteFactory
+    {
+        public SQLiteFactory ()
+        {
+        }
+
+        public SQLiteConnection GetConnection (string fileName)
+        {
+            var path = Path.Combine (Environment.GetFolderPath (Environment.SpecialFolder.Personal), fileName);
+            return new SQLiteConnection (new SQLitePlatformAndroid (), path);
+        }
+    }
+}
+
